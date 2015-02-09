d5f13c985a9b3309274a764a245559d326e41710
Matthew Baggett <matthew@baggett.me>
2015-02-09 18:32:03 +0000

    Remove apc support.. To be replaced with Redis eventually

--- Thru/ActiveRecord/SearchIndex.php
+++ Thru/ActiveRecord/SearchIndex.php
@@ -16,9 +16,6 @@ class SearchIndex
 
     public function put($table, $key, ActiveRecord $object){
       $this->index[$table][$key] = $object;
-      if(function_exists("apc_add")){
-        apc_add("SearchIndex[{$table}][{$key}]", $object, 60);
-      }
       return $this;
     }
 
@@ -33,8 +30,6 @@ class SearchIndex
     public function get($table, $key){
       if($this->exists($table, $key)){
         return $this->index[$table][$key];
-      }elseif(function_exists('apc_exists') && apc_exists("SearchIndex[{$table}][{$key}]")){
-        return apc_fetch("SearchIndex[{$table}][{$key}]");
       }else{
         return false;
       }
@@ -43,9 +38,6 @@ class SearchIndex
     public function expire($table, $key){
       if($this->exists($table, $key)){
         unset($this->index[$table][$key]);
-        if(function_exists('apc_delete')){
-          apc_delete("SearchIndex[{$table}][{$key}]");
-        }
         return true;
       }else{
         return false;
