5e1005c91ff277950455395d846354d9a4cf6ddf
Matthew Baggett <matthew@baggett.me>
2015-02-09 20:48:52 +0000

    Code cleanup in UUID functions

--- Thru/ActiveRecord/UUID.php
+++ Thru/ActiveRecord/UUID.php
@@ -2,7 +2,7 @@
 namespace Thru\ActiveRecord;
 
 class UUID {
-  public static function v3($namespace, $name) {
+  private static function with_namespace($namespace, $name, $crypto, $version){
     if(!self::is_valid($namespace)) return false;
 
     // Get hexadecimal components of namespace
@@ -17,7 +17,16 @@ class UUID {
     }
 
     // Calculate hash value
-    $hash = md5($nstr . $name);
+    $hash = hash($crypto, $nstr . $name);
+
+    switch($version){
+      case '3':
+        $version_byte = 0x3000;
+        break;
+      case '5':
+        $version_byte = 0x5000;
+        break;
+    }
 
     return sprintf('%08s-%04s-%04x-%04x-%12s',
 
@@ -28,8 +37,8 @@ class UUID {
       substr($hash, 8, 4),
 
       // 16 bits for "time_hi_and_version",
-      // four most significant bits holds version number 3
-      (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+      // four most significant bits holds version number 3 or 5
+      (hexdec(substr($hash, 12, 4)) & 0x0fff) | $version_byte,
 
       // 16 bits, 8 bits for "clk_seq_hi_res",
       // 8 bits for "clk_seq_low",
@@ -39,6 +48,10 @@ class UUID {
       // 48 bits for "node"
       substr($hash, 20, 12)
     );
+  }
+
+  public static function v3($namespace, $name) {
+    return self::with_namespace($namespace, $name, 'md5', 3);
   }
 
   public static function v4() {
@@ -65,42 +78,7 @@ class UUID {
   }
 
   public static function v5($namespace, $name) {
-    if(!self::is_valid($namespace)) return false;
-
-    // Get hexadecimal components of namespace
-    $nhex = str_replace(array('-','{','}'), '', $namespace);
-
-    // Binary Value
-    $nstr = '';
-
-    // Convert Namespace UUID to bits
-    for($i = 0; $i < strlen($nhex); $i+=2) {
-      $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
-    }
-
-    // Calculate hash value
-    $hash = sha1($nstr . $name);
-
-    return sprintf('%08s-%04s-%04x-%04x-%12s',
-
-      // 32 bits for "time_low"
-      substr($hash, 0, 8),
-
-      // 16 bits for "time_mid"
-      substr($hash, 8, 4),
-
-      // 16 bits for "time_hi_and_version",
-      // four most significant bits holds version number 5
-      (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
-
-      // 16 bits, 8 bits for "clk_seq_hi_res",
-      // 8 bits for "clk_seq_low",
-      // two most significant bits holds zero and one for variant DCE1.1
-      (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
-
-      // 48 bits for "node"
-      substr($hash, 20, 12)
-    );
+    return self::with_namespace($namespace, $name, 'sha1', 5);
   }
 
   public static function is_valid($uuid) {
