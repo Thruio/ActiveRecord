47f8ce2fdec2137cbd33245100bbb637bf941454
Matthew Baggett <matthew@baggett.me>
2015-04-28 12:58:59 +0100

    Force using socket rather than pipe. Travis is having issues. https://github.com/travis-ci/travis-ci/issues/3781

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ require_once("vendor/autoload.php");
 
 $database = new \Thru\ActiveRecord\DatabaseLayer(array(
   'db_type'     => 'Mysql',
-  'db_hostname' => 'localhost',
+  'db_hostname' => '127.0.0.1',
   'db_port'     => '3306',
   'db_username' => 'travis',
   'db_password' => 'travis',
