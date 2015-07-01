ddcc6a0db0fa7614b1674e866fb7b09678d0fdea
Matthew Baggett <matthew@baggett.me>
2015-07-01 17:55:50 +0100

    Going home :/

---
+++ tests/Models/TestModelNullable.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Thru\ActiveRecord\Test\Models;
+
+use Thru\ActiveRecord\ActiveRecord;
+
+/**
+ * Class TestModel
+ * @var $test_model_id integer
+ * @var $integer_field integer
+ * @var $text_field text
+ * @var $date_field date
+ */
+class TestModel extends ActiveRecord{
+
+  protected $_table = "test_models";
+
+  public $test_model_id;
+  public $integer_field;
+  public $text_field;
+  public $date_field;
+
+}
