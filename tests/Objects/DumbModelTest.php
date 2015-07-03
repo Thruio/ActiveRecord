3e3e32da2c23f2ee6e04096b4137fdd5421e578c
Matthew Baggett <matthew@baggett.me>
2015-07-03 11:16:25 +0100

    Make test regexp a bit less picky.

--- tests/Objects/DumbModelTest.php
+++ tests/Objects/DumbModelTest.php
@@ -52,10 +52,11 @@ class DumbModelTest extends \PHPUnit_Framework_TestCase {
 
   /**
    * @expectedException \Thru\ActiveRecord\DatabaseLayer\TableDoesntExistException
-   * @expectedExceptionMessage 42S02: SQLSTATE[42S02]: Base table or view not found: 1146 Table 'active_record_test.doesntexist' doesn't exist
+   * @expectedExceptionMessageRegExp /42S02: SQLSTATE\[42S02\]: Base table or view not found: 1146 Table '(.*).doesntexist' doesn't exist/
    */
   public function testTableExistsGotcha(){
     $result = DumbModel::queryOne("SELECT * FROM doesntexist", 'Thru\ActiveRecord\Test\Models\NotStdClass');
   }
 }
+
 
