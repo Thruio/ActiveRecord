dd1a3616cf50759ada8f37addb38821b1fae1054
Matthew Baggett <matthew@baggett.me>
2015-07-07 12:50:05 +0100

    Add support for switching env using ENV variables.

--- bootstrap.php
+++ bootstrap.php
@@ -10,14 +10,31 @@ $fileLoggerHandler = new LogHandler\StreamHandler(__DIR__ . "/build/logs/active-
 $monologHandlers = [$fileLoggerHandler];
 $monolog = new Logger("ActiveRecord", $monologHandlers);
 
-$database = new \Thru\ActiveRecord\DatabaseLayer(array(
-  'db_type'     => 'Mysql',
-  'db_hostname' => 'localhost',
-  'db_port'     => '3306',
-  'db_username' => 'travis',
-  'db_password' => 'travis',
-  'db_database' => 'active_record_test',
-));
+if(!getenv('DB')){
+  echo "No DB set in environment\n";
+  exit;
+}
+
+switch(getenv('DB')){
+  case 'mysql':
+    $database = new \Thru\ActiveRecord\DatabaseLayer(array(
+      'db_type' => 'Mysql',
+      'db_hostname' => 'localhost',
+      'db_port' => '3306',
+      'db_username' => 'travis',
+      'db_password' => 'travis',
+      'db_database' => 'active_record_test',
+    ));
+    break;
+  case 'sqlite':
+    $database = new \Thru\ActiveRecord\DatabaseLayer(array(
+      'db_type' => 'Sqlite',
+      'db_file' => 'test.sqlite',
+    ));
+    break;
+  default:
+    die("Unsupported DB: " . getenv('DB') . "\n");
+}
 
 $database->setLogger($monolog);
 
