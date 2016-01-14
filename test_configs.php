8918fb67c34d457bc1d0348eab1d611c07aa4d7f
Matthew Baggett <matthew@baggett.me>
2016-01-14 10:19:21 +0000

    Maybe postgres will be a thing?

--- test_configs.php
+++ test_configs.php
@@ -9,6 +9,15 @@ $testDatabases['mysql'] = array(
   'db_database' => 'active_record_test',
 );
 
+$testDatabases['postgres'] = array(
+  'db_type' => 'pgsql',
+  'db_hostname' => 'localhost',
+  'db_port' => '3306',
+  'db_username' => 'travis',
+  'db_password' => 'travis',
+  'db_database' => 'active_record_test',
+);
+
 $testDatabases['sqlite'] = array(
   'db_type' => 'Sqlite',
   'db_file' => 'test.sqlite',
